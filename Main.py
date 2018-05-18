34b08545defe4ebfcb5b142dc90b12eb3efd5c12
Marius Perle <Marius@FamiliePerle.de>
2018-05-18 13:16:50 +0200

    Add: saves spoken text

--- Main.py
+++ Main.py
@@ -1,6 +1,4 @@
-import atexit
 import configparser
-import os
 import sys
 from threading import Thread
 
@@ -8,6 +6,7 @@ import speech_recognition as sr
 
 
 def mainfunction(source, args):
+    r.adjust_for_ambient_noise(source)
     audio = r.listen(source, phrase_time_limit=2)
     try:
         user = r.recognize_google(audio, language=args['language'])
@@ -17,11 +16,20 @@ def mainfunction(source, args):
     thread.start()
     if args['end_statement']:
         if args['end_statement'] in user:
+            with open('text.txt', 'w', encoding='utf-8') as file:
+                for line in our_text:
+                    if line in ['', ' ']:
+                        continue
+                    if line in args['end_statement']:
+                        continue
+                    file.write(line)
+                    file.write('\n')
             sys.exit('shutdown')
 
 
 def using_it(text):
     print(text)
+    our_text.append(text)
     words = text.split(' ')
     for entry in words:
         if entry in ['', ' ']:
@@ -36,16 +44,15 @@ def using_it(text):
 def read_config():
     config = configparser.ConfigParser()
     config.read('config.ini')
-    end =config['DEFAULT']['end_statement']
+    end = config['DEFAULT']['end_statement']
     if end is '':
         end = None
-    return {'language':config['DEFAULT']['language'],
-            'end_statement':end,}
-
-
+    return {'language': config['DEFAULT']['language'],
+            'end_statement': end, }
 
 
 if __name__ == "__main__":
+    our_text = []
     list_all_words = {}
     r = sr.Recognizer()
     with sr.Microphone() as source:
